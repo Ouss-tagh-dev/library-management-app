584013628f677bc160450e47e1a5df93d5a8ed54
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2025-04-10 18:11:59 +0100

    Add error handling middleware and not found route

--- api-server/app.js
+++ api-server/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { connectDB } = require("./init/db");
 const { authRoute } = require("./routes");
+const { errorHandler } = require("./middlewares");
 
 // Init app
 const app = express();
@@ -18,5 +19,15 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routes
 app.use("/api/v1/auth", authRoute);
+
+// not found route
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler middleware
+app.use(errorHandler);
 
 module.exports = app;
